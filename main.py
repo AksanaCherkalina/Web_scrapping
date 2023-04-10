47679cf128846862956b04db4f2d46e4d68c3635
AksanaCherkalina <101339831+AksanaCherkalina@users.noreply.github.com>
2023-04-11 01:30:50 +0200

    changes

--- main.py
+++ main.py
@@ -1,78 +1,77 @@
-импортировать  json
- запросы на импорт
-импорт  pprintpprint
-из  fake_headers  импортировать  заголовки
-из  bs4  импортировать  BeautifulSoup
+import json
+import pprintpprint
+from fake_headers import Headers
+from bs4 import BeautifulSoup
 
-HOST  =  "https://spb.hh.ru/search/vacancy?text=python&area=1&area=2"
-
-
-защита  get_headers ():
-    вернуть  заголовки ( browser = "firefox" , os = "windows" ). генерировать ()
+HOST = "https://spb.hh.ru/search/vacancy?text=python&area=1&area=2"
 
 
-защита  get_html ():
-    html  =  запросы . получить ( HOST , заголовки = get_headers ()). текст
-    вернуть  html
+def get_headers():
+   return headers(browser="firefox", os="windows").generate()
 
 
-мой_json_list  = []
+def get_html():
+    html = requests.get(HOST, headers=get_headers()).text
+    return html
 
 
-деф  parse_вакансии ():
-    bs  =  BeautifulSoup ( get_html (), "lxml" )
-    список_статей  =  bs . find_all ( class_ = "vacancy-serp-item__layout" )
-    для  я  в  article_list :
-        вакансии  =  я . найти ( "a" , class_ = "serp-item__title" )
-        ссылка  =  вакансии [ "href" ]
-        response_links  =  запросы . получить (
-            ссылка ,
-            заголовки = get_headers (),
-        ). текст
-        bs2  =  BeautifulSoup ( response_links , "lxml" )
-        рабочий_текст  =  bs2 . найти (
-            "див" ,
-            { "data-qa" : "описание вакансии" },
+my_json_list = []
+
+
+def parse_vacancies():
+    bs = BeautifulSoup(get_html(), "lxml")
+    list_articles = bs.find_all(class_="vacancy-serp-item__layout")
+    for article in article_list:
+        vacancies = article.find("a",class_="serp-item__title")
+        link = vacancies["href"]
+        response_links = requests.find(
+            link,
+            headers = get_headers(),
+        ).text
+        bs2 = BeautifulSoup(response_links, "lxml")
+        working_text = bs2.find(
+            "div" ,
+            {"data-qa":"description vacancies"},
         )
-        для  i2  в  work_text :
-            если ( "Фласк"  или  "Джанго" ) в  i2 . текст :
-                имя_компании  =  я . найти ( "а" , class_ = "блоко-ссылка блоко-ссылка_вид-третичный" )
-                город  =  я . найти (
-                    "див" ,
-                    атрибуты = {
+        for i2 in work_text :
+            if ("Flask" or "Django") is i2.text:
+                name_company = article.find("а", class_="bloko-link bloko-link_kind-tertiary")
+                city = article.find(
+                     "div",
+                    attributes = {
                         "data-qa" : "vacancy-serp__vacancy-address" ,
-                        "класс" : "блоко-текст" ,
+                        "class":"bloko-text",
                     },
                 )
-                зарплата  =  я . найти (
-                    "пролет" ,
-                    атрибуты = {
-                        "data-qa" : "вакансия-серп__вакансия-компенсация" ,
-                        "класс" : "блоко-заголовок-раздел-3" ,
+                salary = article.find(
+                    "span",
+                    attributes = {
+                        "data-qa":"vacancy-serp__vacancy-compensation",
+                        "class":"bloko-header-section-3" ,
                     },
                 )
-                если  зарплата :
-                    зарплата  =  зарплата . текст
-                еще :
-                    зарплата  =  "Зарплата не указана"
-                мой_json_list . добавить (
+                if salary:
+                   salary = salary.text
+                else:
+                    salary = "Salary not specified"
+                my_json_list.find(
                     {
-                        "Название компании" : company_name . текст . разделить ( "." ) [ 0 ],
-                        "Название вакансии" : вакансии . текст ,
-                        "Ссылка на вакансию" : ссылка ,
-                        "Город" : город . текст ,
-                        "Вилка заработной платы" : оклад ,
+                        "Название компании" : company_name.text.divide(".")[0],
+                        "Название вакансии" : vacancies.text,
+                        "Ссылка на вакансию" : link,
+                        "Город" : city.text,
+                        "Вилка заработной платы" : salary,
                     }
                 )
-    вернуть  my_json_list
-    # распечатать (my_json_list)
+    return my_json_list
+    # print(my_json_list)
 
 
-защита  write_json ( json_list ):
-    с  открытым ( "Web_scrapping/vacancy.json" , "w" , encoding = "utf-8" ) как  f :
-        json . дамп ( json_list , f , отступ = 2 , обеспечить_ascii = False )
+def write_json(json_list):
+    with open("Web_scrapping/vacancy.json", "w", encoding="utf-8") as f:
+        json.dump(json_list, f, indent=2, provide_ascii=False)
 
 
-если  __name__  ==  "__main__" :
-    запустить  =  разбор_вакансии ()
-    write_json ( выполнить )
+if __name__ == "__main__":
+    run = parsing_vacancies()
+    write_json(execute)
