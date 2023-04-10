cd29d29f9b915a14f3aaff379fe935ccf8486227
AksanaCherkalina <101339831+AksanaCherkalina@users.noreply.github.com>
2023-04-11 01:34:24 +0200

    changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-import pprintpprint
+import requests
+import pprint
 from fake_headers import Headers
 from bs4 import BeautifulSoup
 
@@ -7,7 +8,7 @@ HOST = "https://spb.hh.ru/search/vacancy?text=python&area=1&area=2"
 
 
 def get_headers():
-   return headers(browser="firefox", os="windows").generate()
+    return Headers(browser="firefox", os="windows").generate()
 
 
 def get_html():
@@ -18,49 +19,49 @@ def get_html():
 my_json_list = []
 
 
-def parse_vacancies():
+def parse_vacancy():
     bs = BeautifulSoup(get_html(), "lxml")
-    list_articles = bs.find_all(class_="vacancy-serp-item__layout")
-    for article in article_list:
-        vacancies = article.find("a",class_="serp-item__title")
+    article_list = bs.find_all(class_="vacancy-serp-item__layout")
+    for i in article_list:
+        vacancies = i.find("a", class_="serp-item__title")
         link = vacancies["href"]
-        response_links = requests.find(
+        response_links = requests.get(
             link,
-            headers = get_headers(),
+            headers=get_headers(),
         ).text
         bs2 = BeautifulSoup(response_links, "lxml")
-        working_text = bs2.find(
-            "div" ,
-            {"data-qa":"description vacancies"},
+        work_text = bs2.find(
+            "div",
+            {"data-qa": "vacancy-description"},
         )
-        for i2 in work_text :
-            if ("Flask" or "Django") is i2.text:
-                name_company = article.find("а", class_="bloko-link bloko-link_kind-tertiary")
-                city = article.find(
-                     "div",
-                    attributes = {
-                        "data-qa" : "vacancy-serp__vacancy-address" ,
-                        "class":"bloko-text",
+        for i2 in work_text:
+            if ("Flask" or "Django") in i2.text:
+                company_name = i.find("a", class_="bloko-link bloko-link_kind-tertiary")
+                city = i.find(
+                    "div",
+                    attrs={
+                        "data-qa": "vacancy-serp__vacancy-address",
+                        "class": "bloko-text",
                     },
                 )
-                salary = article.find(
+                salary = i.find(
                     "span",
-                    attributes = {
-                        "data-qa":"vacancy-serp__vacancy-compensation",
-                        "class":"bloko-header-section-3" ,
+                    attrs={
+                        "data-qa": "vacancy-serp__vacancy-compensation",
+                        "class": "bloko-header-section-3",
                     },
                 )
                 if salary:
-                   salary = salary.text
+                    salary = salary.text
                 else:
-                    salary = "Salary not specified"
-                my_json_list.find(
+                    salary = "Зарплата не указана"
+                my_json_list.append(
                     {
-                        "Название компании" : company_name.text.divide(".")[0],
-                        "Название вакансии" : vacancies.text,
-                        "Ссылка на вакансию" : link,
-                        "Город" : city.text,
-                        "Вилка заработной платы" : salary,
+                        "Название компании": company_name.text.split(".")[0],
+                        "Название вакансии": vacancies.text,
+                        "Ссылка на вакансию": link,
+                        "Город": city.text,
+                        "Вилка заработной платы": salary,
                     }
                 )
     return my_json_list
@@ -69,9 +70,9 @@ def parse_vacancies():
 
 def write_json(json_list):
     with open("Web_scrapping/vacancy.json", "w", encoding="utf-8") as f:
-        json.dump(json_list, f, indent=2, provide_ascii=False)
+        json.dump(json_list, f, indent=2, ensure_ascii=False)
 
 
 if __name__ == "__main__":
-    run = parsing_vacancies()
-    write_json(execute)
+    run = parse_vacancy()
+    write_json(run)
